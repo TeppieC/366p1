c9cb85ae428d1b7df794a331b12afb334170d45a
TeppieC <teppie.c@hotmail.com>
2016-10-31 16:40:46 -0600

    finished doubleQ

--- doubleQ.py
+++ doubleQ.py
@@ -1,13 +1,14 @@
 import blackjack
 from pylab import *
-from numpy import *
+import numpy as np
 
 
-def main(epsilon, numEpisodes):
+def main(epsilon, numEpisodes, ):
     repnums=100
     global Q1, Q2
     Q1=0.0000001*rand(181,2) # initialize the estimates
     Q2=0.0000001*rand(181,2) # initialize the estimates
+
     for repnum in range(repnums):
         returnSum = 0.0
         for episodeNum in range(numEpisodes):
@@ -15,19 +16,24 @@ def main(epsilon, numEpisodes):
             stepsize=0.001
             discount=1.0
             state=blackjack.init()
-            while(state!=False):
+            while(True):
                 action=chooseAction(state, epsilon)
                 reward,statenext=blackjack.sample(state,action)
-                if randint(0,2):
+                if np.random.randint(0,2):
                     Q1[state][action]=Q1[state][action]+stepsize*(reward+discount*Q2[statenext][argmax(Q1[statenext])]-Q1[state][action])
                 else:                    
                     Q2[state][action]=Q2[state][action]+stepsize*(reward+discount*Q1[statenext][argmax(Q2[statenext])]-Q2[state][action])
                 state=statenext
-                G=G+reward
+                G=G+discount*reward
+                
+                if state==False:
+                    break
             #print "Episode: ", episodeNum, "Return: ", G
             returnSum = returnSum + G
+
         #allSum=allSum+G
         print ("Average return every numEpisodes: ", returnSum/numEpisodes)
+
     #after learining the policy
     final_sum=0.0
     for episodeNum in range(repnums*numEpisodes):
@@ -41,17 +47,17 @@ def main(epsilon, numEpisodes):
     blackjack.printPolicy(learning)
 
 def chooseAction(state, epsilon):
-    num=random.random()
+    num=np.random.random()
     if (num>=epsilon):
         return argmax(Q1[state]+Q2[state])
     else:
-        return (random.randint(0,2))
+        return (np.random.randint(0,2))
 
 def learning(state):
-    return argmax(Q[state])
+    return argmax(Q1[state]+Q2[state])
 
 if __name__ == "__main__":
     epsilon = 1
     main(epsilon, 100)
-    epsilon = 0.001
+    #epsilon = 0.001
     #main(epsilon, 1000000)
--- randomPolicy.py
+++ randomPolicy.py
@@ -1,12 +1,19 @@
 import blackjack
 from pylab import *
+import numpy as np
 
 numEpisodes = 2000
 
 returnSum = 0.0
-for episodeNum in range(numEpisodes):
+for episodeNum in range(numEpisodes): #loop for 2000 iterations
     G = 0
-    ...
+    state=blackjack.init()
+    while(True): # if not reach the terminal state
+        reward,state=blackjack.sample(state,np.random.randint(0,2))
+        G=G+reward
+        if state==False:
+        	break
+
     print("Episode: ", episodeNum, "Return: ", G)
     returnSum = returnSum + G
 print("Average return: ", returnSum/numEpisodes)
